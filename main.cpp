b6b4f87167f7c14aef8c7faff1247123c39d1d17
Andres <andresll706@gmail.com>
2020-05-05 19:11:41 +0200

    holi

--- main.cpp
+++ main.cpp
@@ -21,69 +21,7 @@ int main() {
     RectangleShape coliAbV;
     RectangleShape coliIzV;
     RectangleShape coliDcV;
-/*
-    // ESTO VA EN MOTOR---
-    Font fuente; 
-    if(!fuente.loadFromFile("resources/fuentes/AnotherRound.otf")){
-      cout<< "NO SE CARGA"<<endl;
-    }
-    //.-------
-    menu men(fuente, 3); // crear  menu 
-    men.cambiarTexto(0, sf::Color::Red, "PLAY" , 540 , 320); // Fachada
-    men.cambiarTexto(1, sf::Color::White, "OPTIONS" , 540, 350); 
-    men.cambiarTexto(2, sf::Color::White, "EXIT" , 540 , 380); 
-    while (m->getVentana()->isOpen()) {
-      //Bucle de obtención de eventos
-        Event event;
-        while (m->getVentana()->pollEvent(event)) {
 
-          switch (event.type){
-            case Event::Closed:
-              m->getVentana()->close();
-            break;
-            
-            case Event::KeyPressed:
-              //Verifico si se pulsa alguna tecla de movimiento
-              switch (event.key.code) {
-                case Keyboard::Up:
-                  men.moveUp();
-                break;
-                case Keyboard::Down:
-                  men.moveDown();
-                break;
-                case Keyboard::Return:
-                  switch (men.getItemPressed()){
-                    case 0:
-                        cout<<"eligo play" << endl;
-                    break;
-                    
-                    case 1: 
-                        cout<<"eligio opciones"<<endl;
-                      break;
-                    case 2:
-                        cout<<"eligo salir"<<endl;
-                        m->getVentana()->close();
-                      break;
-                  }
-                break;
-                default:
-                break;
-                
-              }
-          
-            default: 
-            break;  
-          }
-        }
-
-        m->getVentana()->clear();
-        men.render();
-        m->getVentana()->display();
-    }
-
-*/
-
-    //funcion inicializar juego de juego VERY WIP SUCH WOW
 
     while(m->getOpen()){
 
@@ -101,18 +39,7 @@ int main() {
         case sf::Event::EventType::KeyPressed:
 
         switch (event.key.code) {
-          /*Sif(sf::Keyboard::isKeyPressed(sf::Keyboard::X)){ //WIP FACHADA y LECTURA TECLADO
-      jugador->morir();
-  }
-  if(sf::Keyboard::isKeyPressed(sf::Keyboard::G)){ //WIP FACHADA y LECTURA TECLADO
-      jugador->godMode();
-  }
-  if(sf::Keyboard::isKeyPressed(sf::Keyboard::R)){ //WIP FACHADA y LECTURA TECLADO
-      
-  }
-  if(sf::Keyboard::isKeyPressed(sf::Keyboard::N)){ //WIP FACHADA y LECTURA TECLADO
-      
-  }*/
+
         case sf::Keyboard::P:
           instancia->getJugador()->obtenerPU_SaltoDoble();
           instancia->getJugador()->obtenerPU_Slowhits();
@@ -152,22 +79,6 @@ int main() {
       elapsus = m->getRelojM();
 
 
-      // coliArV.setPosition( instancia->getJugador()->coliArriba.left , instancia->getJugador()->coliArriba.top );
-      // coliArV.setFillColor(Color(255, 255, 0, 128));
-      // coliArV.setSize( Vector2f( instancia->getJugador()->coliArriba.width, instancia->getJugador()->coliArriba.height ) );
-
-      // coliAbV.setPosition( instancia->getJugador()->coliAbajo.left , instancia->getJugador()->coliAbajo.top );
-      // coliAbV.setFillColor(Color(0,0,0, 128));
-      // coliAbV.setSize( Vector2f( instancia->getJugador()->coliAbajo.width, instancia->getJugador()->coliAbajo.height ) );
-      
-      // coliIzV.setPosition( instancia->getJugador()->coliIzquierda.left , instancia->getJugador()->coliIzquierda.top );
-      // coliIzV.setFillColor(Color(255, 0, 0, 128));
-      // coliIzV.setSize( Vector2f( instancia->getJugador()->coliIzquierda.width, instancia->getJugador()->coliIzquierda.height ) );
-
-      // coliDcV.setPosition( instancia->getJugador()->coliDerecha.left , instancia->getJugador()->coliDerecha.top );
-      // coliDcV.setFillColor(Color(0, 0, 255, 128));
-      // coliDcV.setSize( Vector2f( instancia->getJugador()->coliDerecha.width, instancia->getJugador()->coliDerecha.height ) );
-
       window->clear();
       
       if(elapsus >= UPDATE_TICK_TIME){
@@ -178,11 +89,6 @@ int main() {
          instancia->render(elapsus/UPDATE_TICK_TIME);
       }
       
-     /* window->draw(coliDcV);
-      window->draw(coliIzV);
-      window->draw(coliArV);
-      window->draw(coliAbV);*/
-
       window->display();
 
       
