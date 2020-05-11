e6fa4a5b01f5828e21e2d031d3950e04e0d86d6c
Vermia <domeofvermia@outlook.com>
2020-05-11 21:19:23 +0200

    comentarios

--- Juego/Final.h
+++ Juego/Final.h
@@ -11,17 +11,16 @@ private:
     static Final* p_instancia;
     int graduaColor; //para el flash que pone la pantalla blanca
     bool dandoVueltas; //si estamos en el estado de dando vueltas o ya se ha terminado
-    bool modRadio; 
-    float angulos[numEstrellas];
-    float radio;
+    bool modRadio; //controla que las estrellas se acerquen o alejen del mago
+    float angulos[numEstrellas]; //angulos de las estrellas, para moverlas
+    float radio; //distancia entre estrellas y mago
     Cuerpo *estrellas[numEstrellas];
     Cuerpo *mago;
     sf::View* vista;
-    Text *puntuacion;
-    Text *the_end;
+    Text *puntuacion; //incluye el "the end", por lo demas es igual que en Transicion
     Font *fuente;
-    int vueltas;
-    float finishDelay;
+    int vueltas; //cantidad de vueltas que han dado las estrellas
+    float finishDelay; //tiempo que tarda en enviarte al menu principal
 public:
     static Final* instancia();
     ~Final();
